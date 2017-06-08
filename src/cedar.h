dlazesz/cedar

commit 327476fa8480cf3a4b40be6becc601445c0af8f0

Enable storing really huge amount (> 10^9) of keys

src/cedar.h
@@ -25,7 +25,7 @@ namespace cedar {
   typedef unsigned char  uchar;
   template <typename T> struct NaN { enum { N1 = -1, N2 = -2 }; };
   template <> struct NaN <float> { enum { N1 = 0x7f800001, N2 = 0x7f800002 }; };  // 0x7f800001 == +INF +1 and 0x7f800002 == +INF +2
-  static const int MAX_ALLOC_SIZE = 1 << 16; // must be divisible by 256 (1 << 16 == 65536 == 256*256)
+  static const int MAX_ALLOC_SIZE = 1 << 64; // must be divisible by 256 (1 << 16 == 65536 == 256*256, 1 << 64 == more than enough :P )
 
   // dynamic double array
   template <typename value_type,
@@ -35,10 +35,10 @@ namespace cedar {
             const int     MAX_TRIAL = 1,
             const size_t  NUM_TRACKING_NODES = 0>
   class da {
-      typedef int baseindex;
-      typedef int checkindex;
-      typedef int size_type;
-      typedef int blockindex;
+      typedef long baseindex;
+      typedef long checkindex;
+      typedef long size_type;
+      typedef long blockindex;
       typedef union { baseindex i; value_type x; } nodeelement;
   public:
     enum error_code { CEDAR_NO_VALUE = NO_VALUE, CEDAR_NO_PATH = NO_PATH, CEDAR_VALUE_LIMIT = 2147483647 };  // 2147483647 == 2^31 − 1

src/mkcedar.cc
@@ -3,7 +3,6 @@
 // Copyright (c) 2013-2014 Naoki Yoshinaga <ynaga@tkl.iis.u-tokyo.ac.jp>
 #include <cstdio>
 #include <cstdlib>
-#define USE_REDUCED_TRIE 1
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>

commit 288484e3199df6b5bc81bd2951a4e44328b8f556

Fix segfault with reduced trie for long type introduced in 327476f

src/cedar.h
@@ -35,7 +35,7 @@ namespace cedar {
             const int     MAX_TRIAL = 1,
             const size_t  NUM_TRACKING_NODES = 0>
   class da {
-      typedef long baseindex;
+      typedef long baseindex;  // XXX This type is associated with value_type maybe there is more!
       typedef long checkindex;
       typedef long size_type;
       typedef long blockindex;
@@ -113,7 +113,7 @@ namespace cedar {
     };
     //
     da () : tracking_node (), _array (0), _ninfo (0), _block (0), _bheadF (0), _bheadC (0), _bheadO (0), _capacity (0), _size (0), _no_delete (false), _reject () {
-      static_assert (sizeof (value_type) <= sizeof (int), "value type is not supported maintain_a value array by yourself and store its index");
+      static_assert (sizeof (value_type) <= sizeof (baseindex), "value type is not supported maintain a value array by yourself and store its index");
       _initialize ();
     }
     //
@@ -493,7 +493,7 @@ namespace cedar {
     short      _reject[257];
     //
     static void _err (const char* fn, const size_t ln, const char* msg){
-      std::fprintf (stderr, "cedar: %s [%lu]: %s", fn, ln, msg); std::exit (1); }
+      std::fprintf (stderr, "cedar: %s [%zu]: %s", fn, ln, msg); std::exit (1); }
     //
     template <typename T>
     static void _realloc_array (T*& p, const size_t size_n, const size_type size_p = 0) {
@@ -519,7 +519,7 @@ namespace cedar {
       _block[0].ehead = 1; // bug fix for erase
       _capacity = _size = 256;
       for (size_t i = 0 ; i <= NUM_TRACKING_NODES; ++i) tracking_node[i] = 0;
-      for (short i = 0; i <= 256; ++i) _reject[i] = i + 1;
+      for (short i = 1; i <= 257; ++i) _reject[i-1] = i;  // This version do not cast i + 1 up to int
     }
     // follow/create edge
     template <typename T>
@@ -555,7 +555,7 @@ namespace cedar {
     //
 #ifndef USE_FAST_LOAD
     void _restore_ninfo () {
-      _realloc_array (_ninfo, _size);
+      _realloc_array (_ninfo, static_cast<size_t>(_size));
       for (size_type to = 0; to < _size; ++to) {
         const checkindex from = _array[to].check;
         if (from < 0) continue; // skip empty node
@@ -567,7 +567,7 @@ namespace cedar {
     }
     //
     void _restore_block () {
-      _realloc_array (_block, _size >> 8);
+      _realloc_array (_block, static_cast<size_t>(_size) >> 8);
       _bheadF = _bheadC = _bheadO = 0;
       blockindex bi (0);
       size_type e (0);
@@ -621,9 +621,9 @@ namespace cedar {
 #else
         _capacity += _capacity; // Double capacity
 #endif
-        _realloc_array (_array, _capacity, _capacity);
-        _realloc_array (_ninfo, _capacity, _size);
-        _realloc_array (_block, _capacity >> 8, _size >> 8); // _capacity / 256, _size / 256 to match blocksize == 256
+        _realloc_array (_array, static_cast<size_t>(_capacity), _capacity);
+        _realloc_array (_ninfo, static_cast<size_t>(_capacity), _size);
+        _realloc_array (_block, static_cast<size_t>(_capacity) >> 8, _size >> 8); // _capacity / 256, _size / 256 to match blocksize == 256
       }
       _block[_size >> 8].ehead = _size;
       _array[_size] = node (- (_size + 255),  - (_size + 1));
@@ -767,7 +767,10 @@ namespace cedar {
       for (const uchar* p = first; p <= last; ++p) { // to_ => to
         const baseindex to  = _pop_enode (base, *p, from);
         const baseindex to_ = base_ ^ *p;
-        _ninfo[to].sibling = (p == last ? 0 : *(p + 1));
+
+        if (p == last) _ninfo[to].sibling = 0;  // This is more readable
+        else _ninfo[to].sibling = *(p + 1);
+
         if (flag && to_ == to_pn) continue; // skip newcomer (no child)
         cf (to_, to); // user-defined callback function to handle moved nodes
         node& n  = _array[to];

src/mkcedar.cc
@@ -18,7 +18,7 @@ int main (int argc, char **argv) {
   if (argc < 3)
     { std::fprintf (stderr, "Usage: %s keys trie\n", argv[0]); std::exit (1); }
   //
-  cedar::da <int> trie;
+  cedar::da <long> trie;
   int n = 0;
   FILE* fp = argv[1][0] == '-' ? stdin : std::fopen (argv[1], "r");
   char line[8192];
@@ -29,7 +29,7 @@ int main (int argc, char **argv) {
   if (trie.save (argv[2]) != 0)
     { std::fprintf (stderr, "cannot save trie: %s\n", argv[2]); std::exit (1); }
   //
-  std::fprintf (stderr, "keys: %ld\n", trie.num_keys ());
+  std::fprintf (stderr, "keys: %zu\n", trie.num_keys ());
   std::fprintf (stderr, "size: %ld\n", trie.size ());
   std::fprintf (stderr, "nonzero_size: %ld\n", trie.nonzero_size ());
   return 0;